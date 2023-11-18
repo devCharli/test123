5601b74cf11b626a67a36a02b2156e757b747e97
devCharli <devcharli@gmail.com>
2023-11-18 14:37:41 -0800

    update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  distDir: "build", //this line will tell the build to create a file with this name
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
